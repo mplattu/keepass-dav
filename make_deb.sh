45301b32198bda5e028f9657fe9da593fec8645c
Matti Lattu <matti.lattu@iki.fi>
2019-03-21 23:52:24 +0200

    Initial version

---
+++ make_deb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+VERSION=1.0.0
+
+if [ -d deb-root ]; then
+	rm -fR deb-root
+fi
+
+mkdir deb-root
+
+mkdir -p deb-root/usr/local/bin/
+cp keepass-dav deb-root/usr/local/bin/
+chmod 755 deb-root/usr/local/bin/
+
+mkdir -p deb-root/usr/share/applications/
+cp keepass-dav.desktop deb-root/usr/share/applications/
+
+mkdir -p deb-root/etc/
+cp keepass-dav.etc deb-root/etc/keepass-dav
+
+fpm -C deb-root/ -s dir --name keepass-dav --architecture native -t deb \
+	--version "$VERSION" --depends zenity --depends keepassx --depends curl \
+	.
